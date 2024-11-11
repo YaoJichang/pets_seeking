1af92087f9a2f20b1e13df8e978431f51ef062e3
yaojichang <1148968600@qq.com>
2024-11-11 10:52:20 +0800

    bug fix+添加商铺类型的查询缓存

--- src/main/java/com/hmdp/controller/ShopTypeController.java
+++ src/main/java/com/hmdp/controller/ShopTypeController.java
@@ -1,15 +1,25 @@
 package com.hmdp.controller;
 
 
+import cn.hutool.core.lang.UUID;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.service.IShopTypeService;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TYPE_KEY;
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TYPE_TTL;
 
 /**
  * <p>
@@ -24,11 +34,35 @@ import java.util.List;
 public class ShopTypeController {
     @Resource
     private IShopTypeService typeService;
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
+    //todo 练习：给店铺类型查询业务添加缓存
     @GetMapping("list")
     public Result queryTypeList() {
-        List<ShopType> typeList = typeService
+        // 1、从Redis中查询店铺类型
+        String key = CACHE_SHOP_TYPE_KEY + UUID.randomUUID().toString(true);
+        String shopTypeJSON = stringRedisTemplate.opsForValue().get(key);
+
+        List<ShopType> typeList = null;
+        // 2、判断缓存是否命中
+        if (StrUtil.isNotBlank(shopTypeJSON)) {
+            // 2.1 缓存命中，直接返回缓存数据
+            typeList = JSONUtil.toList(shopTypeJSON, ShopType.class);
+            return Result.ok(typeList);
+        }
+        // 2.1 缓存未命中，查询数据库
+            typeList = typeService
                 .query().orderByAsc("sort").list();
+
+        // 3、判断数据库中是否存在该数据
+        if (Objects.isNull(typeList)) {
+            // 3.1 数据库中不存在该数据，返回失败信息
+            return Result.fail("店铺类型不存在");
+        }
+        // 3.2 店铺数据存在，写入Redis，并返回查询的数据（设置了数据有效期)
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(typeList),
+                CACHE_SHOP_TYPE_TTL, TimeUnit.MINUTES);
         return Result.ok(typeList);
     }
 }
