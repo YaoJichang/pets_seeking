4ebfc0dc1ab7c65614ed9b13c2c47cd31fdbad91
yaojichang <1148968600@qq.com>
2024-12-13 18:07:48 +0800

    乐观锁解决优惠券库存超卖问题（还写了一些知识点的注释）

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -51,7 +51,19 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
             return  Result.fail("库存不足");
         }
     //5.扣减库存
-        seckillVoucherService.update().setSql("stock = stock -1").eq("voucher_id",voucherId).update();
+       boolean success =  seckillVoucherService.update()
+                .setSql("stock = stock -1")
+                .eq("voucher_id",voucherId)
+                .gt("stock",0) //where id = ? and stock > 0
+             //   .eq("stock",voucher.getStock()) // where id = ? and stock = ? ---->这是用的CAS的方法 但是库存比较特殊 用上面的与0比较更好 不会出现库存还有但是大家都抢不到的情况
+                .update();
+        //知识点：如果有些方案里面乐观锁要锁的不是库存，它只能通过数据有没有变化来去判断是否安全，要提高乐观存的成功率的话 可以采用【分批加锁】或者叫【分段锁】的方案--->把资源分表存放 比如100个资源 分到10张表中，每张表是10 抢的时候可以去多张表分别去抢 成功率会提高（这种思想在hashmap中有实现
+
+        if(!success){
+            //扣减失败v
+            return Result.fail("库存不足！");
+        }
+
     //6.创建订单
         VoucherOrder order = new VoucherOrder();
         //6.1 订单id
